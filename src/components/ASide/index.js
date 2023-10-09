96872623289faf097155ea2302e5a7ecdcb89985
Matheus <matheusr@lacunasoftware.com>
2023-10-08 21:46:59 -0300

    Adaptando ASide para o mobile

--- src/components/ASide/index.js
+++ src/components/ASide/index.js
@@ -1,9 +1,8 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 import { makeStyles } from "@material-ui/core/styles";
-import { Drawer, List, ListItem, ListItemText } from "@material-ui/core";
+import { Drawer, List, ListItem, ListItemText, Button } from "@material-ui/core";
 
 import Avatar from "@mui/material/Avatar";
 
@@ -23,11 +22,12 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const ASide = () => {
+  const [open, setOpen] = useState(false);
+
   const profilePicURI = useSelector((state) => state.user.profilePicURI);
   const userName = useSelector((state) => state.user.userName);
-  const codeforcesRanking = useSelector(
-    (state) => state.user.codeforcesRanking
-  );
+ 
+  const codeforcesRanking = useSelector((state) => state.user.codeforcesRanking);
   const atcoderRanking = useSelector((state) => state.user.atcoderRanking);
   const codechefRanking = useSelector((state) => state.user.codechefRanking);
   const spojRanking = useSelector((state) => state.user.spojRanking);
@@ -35,7 +35,7 @@ const ASide = () => {
 
   const classes = useStyles();
 
-  return (
+  const site = () =>
     <Drawer
       className={classes.drawer}
       variant="permanent"
@@ -81,6 +81,64 @@ const ASide = () => {
         </ListItem>
       </List>
     </Drawer>
+
+  return (
+    <>
+      <div className={css.sit}>
+        {site()}
+      </div>
+      <div className={css.mob}>
+        <Button className={css.openBtn} onClick={() => setOpen(true)}>Abrir {String(open)}</Button>
+        <Drawer
+          className={classes.drawer}
+          anchor="left"
+          open={open}
+          onClose={() => setOpen(false)}
+          variant="temporary"
+          classes={{
+            paper: classes.drawerPaper,
+          }}
+        >
+          <div className={classes.toolbar} />
+          <List>
+            <ListItem className={css.avatar}>
+              <Avatar
+                alt="avatarURI"
+                src={profilePicURI}
+                sx={{ width: 60, height: 60 }}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="User Name: " secondary={userName} />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Codeforces Rating: "
+                secondary={codeforcesRanking}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Atcoder Rating: " secondary={atcoderRanking} />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Online Judge AvgDacu: "
+                secondary={uvaAvgDacu}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="SPOJ Ranking: " secondary={spojRanking} />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Codechef Rating: "
+                secondary={codechefRanking}
+              />
+            </ListItem>
+          </List>
+        </Drawer>
+      </div>
+    </>
   );
 };
 
